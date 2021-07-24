68aecd79aff3c12ad4b6409ac468b8b0a8f6bb7e
Prabal Agrawal <66802603+prabal03@users.noreply.github.com>
2021-07-25 00:12:10 +0530

    Delete script.sh

--- master/files/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo mkdir admin/
-
-sudo cp /etc/kubernetes/admin.conf admin/
-
-sudo yum install git -y
- 
-cd admin/;sudo git init;sudo git add .;sudo git commit -m 'version 1' .;sudo git push https://prabal03:prabalagrawal0307@github.com/prabal03/k8s-confile.git --all
